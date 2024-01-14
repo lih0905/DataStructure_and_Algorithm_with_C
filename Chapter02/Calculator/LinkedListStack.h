968e2530203a8d8170d5750c1fcd5aacb897edc6
Inhwan Lee <lih0905@gmail.com>
2024-01-14 22:25:50 +0900

    Finish Chapter02

---
+++ Chapter02/Calculator/LinkedListStack.h
@@ -0,0 +1,33 @@
+#ifndef LINKEDLISTSTACK_H
+#define LINKEDLISTSTACK_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct tagNode
+{
+    char *Data;
+    struct tagNode *NextNode;
+} Node;
+
+typedef struct tagLinkedListStack
+{
+    Node *List;
+    Node *Top;
+} LinkedListStack;
+
+void LLS_CreateStack(LinkedListStack **Stack);
+void LLS_DestroyStack(LinkedListStack *Stack);
+
+Node *LLS_CreateNode(char *Data);
+void LLS_DestroyNode(Node *_Node);
+
+void LLS_Push(LinkedListStack *Stack, Node *NewNode);
+Node *LLS_Pop(LinkedListStack *Stack);
+
+Node *LLS_Top(LinkedListStack *Stack);
+int LLS_GetSize(LinkedListStack *Stack);
+int LLS_IsEmpty(LinkedListStack *Stack);
+
+#endif
