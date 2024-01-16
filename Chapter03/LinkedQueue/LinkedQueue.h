a761828ac3c6a341569f951c77526e068bb492a5
Inhwan Lee <lih0905@gmail.com>
2024-01-16 19:06:01 +0900

    Finish Chapter03

---
+++ Chapter03/LinkedQueue/LinkedQueue.h
@@ -0,0 +1,32 @@
+#ifndef LINKEDQUEUE_H
+#define LINKEDQUEUE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct tagNode
+{
+    char *Data;
+    struct tagNode *NextNode;
+} Node;
+
+typedef struct tagLinkedQueue
+{
+    Node *Front;
+    Node *Rear;
+    int Count;
+} LinkedQueue;
+
+void LQ_CreateQueue(LinkedQueue **Queue);
+void LQ_DestroyQueue(LinkedQueue *Queue);
+
+Node *LQ_CreateNode(char *NewData);
+void LQ_DestroyNode(Node *_Node);
+
+void LQ_Enqueue(LinkedQueue *Queue, Node *NewNode);
+Node *LQ_Dequeue(LinkedQueue *Queue);
+
+int LQ_IsEmpty(LinkedQueue *Queue);
+
+#endif
