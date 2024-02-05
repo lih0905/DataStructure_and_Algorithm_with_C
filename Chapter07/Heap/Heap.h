cd2591afbd707ce6ea3bf84c6553c995f13d3999
Inhwan Lee <lih0905@gmail.com>
2024-02-05 22:26:34 +0900

    Finish Heap

---
+++ Chapter07/Heap/Heap.h
@@ -0,0 +1,29 @@
+#ifndef HEAD_H
+#define HEAD_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
+
+typedef int ElementType;
+
+typedef struct tagHeapNode {
+    ElementType Data;
+} HeapNode;
+
+typedef struct tagHeap {
+    HeapNode* Nodes;
+    int Capacity;
+    int UsedSize;
+} Heap;
+
+Heap* HEAP_Create(int InitialSize);
+void HEAP_Destroy(Heap* H);
+void HEAP_Insert(Heap* H, ElementType NewData);
+void HEAP_DeleteMin(Heap* H, HeapNode* Root);
+int HEAP_GetParent(int Index);
+int HEAP_GetLeftChild(int Index);
+void HEAP_SwapNodes(Heap* H, int Index1, int Index2);
+void HEAP_PrintNodes(Heap* H);
+
+#endif
